ace8a37c248ff855ca340e1430d1bfbd7df3fabe
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2023-08-27 00:09:03 +0300

    Directives

--- AngularBasics/src/app/notification/notification.component.ts
+++ AngularBasics/src/app/notification/notification.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="alert alert-success align" [hidden]="displayNotification" >
+  template: `<div class="alert alert-success align" [ngClass]="{fadeOut: displayNotification}" >
     This website uses cookies to provide better user experience.
     <div class="close"><button class="btn" (click)="closeNotification()">X</button></div>
     </div>`,
   styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; background-color: #fad7a0 ; text-align: center;}",
-          "p{font-size: 14px;}", ".align{text-align: center; font-weight: bold}",
-        ".close{float:right; margin-top: -15px;}"]
+          "p{font-size: 14px;}", ".align{text-align: center; font-weight: bold; margin-top: 10px}",
+          ".close{float:right; margin-top: -15px;}",
+          ".fadeOut{visibility: hidden; opacity:0; transition: visibility 0s 2s, opacity 2s  linear;}"]
 })
 export class NotificationComponent {
 
